ee2e627825348ebf8ff904c46b6746439ddd9f50
Seraffy <gabrielsantos.s2018@outlook.com>
2023-11-29 07:49:16 -0300

    Construindo a Classe Jogador

--- RoubaMonte/Program.cs
+++ RoubaMonte/Program.cs
@@ -6,14 +6,16 @@ namespace RoubaMonte
     {
         static void Main(string[] args)
         {
-            // new MenuView().MostrarMenu();
+            Jogador jogador = new Jogador();
+            jogador.MostrarJogadoresExistentes();
+            /*  new MenuView().MostrarMenu();
             Baralho B = new Baralho();
             var pilha = B.GerarBaralho(2);
 
             foreach (var p in pilha)
             {
                 Console.WriteLine(p.Numero+" "+p.Nipe);
-            }
+            } */
         }
     }
 }
--- RoubaMonte/src/model/Jogador.cs
+++ RoubaMonte/src/model/Jogador.cs
@@ -11,18 +11,37 @@ class Jogador {
 
 
     public string [] GetPlayersToMatriz(){
-        String [] jogadores = File.ReadAllLines(System.IO.Directory.GetCurrentDirectory() + "/src/model/jogador.txt");
+        string [] jogadores = File.ReadAllLines(System.IO.Directory.GetCurrentDirectory() + "/src/model/jogador.txt");
         return jogadores;
     }
+    
+    //Function to init new Player on table
     public Jogador(){
         string [] players = GetPlayersToMatriz();
-        for(int i =0; i< players.Length;i++){
-            id = (players.Length).ToString();
-            Console.WriteLine(id);
-        }
-        
+        id = players.Length.ToString();
+        Console.WriteLine("Digite o nome do Jogador");
+        name = Console.ReadLine();
     }
     
+    //Funtion to Show Players that already played once
+    public void  MostrarJogadoresExistentes(){
+        string [] players = GetPlayersToMatriz();
+        for(int i=0; i<players.Length;i++){
+            string  sub = players[i];
+            string [] playerIdName = sub.Split(',');
+            Console.Write("Player ID: {0}",playerIdName[0]);
+            Console.Write("   ");
+            Console.Write("Player NAME: {0}", playerIdName[1]);
+            Console.WriteLine("");
+        }
+    }
+
+
+    //Function To Select a Player that alredy played 
+    
+    /* public string [] PickPlayer(){
+
+    } */
     public void SavePlayersInTxt(string [] players){
 
     }
